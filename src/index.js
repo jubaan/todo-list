e20882000ef4e1c66fd23ea71c4e6f399a9bb6e7
jubaan <julio.ab@outlook.com>
2020-12-13 19:50:09 -0600

    Update index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import '../node_modules/materialize-css/dist/js/materialize.min';
-import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import './main.css';
+import todoList from './todoList';
+import addItem from './addItem';
+import removeItem from './removeItem';
+import searchItem from './searchItem';
+
+const content = document.querySelector('.content');
+
+content.innerHTML = new todoList().render();
+
+addItem();
+removeItem();
+searchItem();
+
