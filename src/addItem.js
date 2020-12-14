1e33a8a5fdc307f14c40f2d371ed59d6c64b628d
jubaan <julio.ab@outlook.com>
2020-12-13 19:46:34 -0600

    Add additem

---
+++ src/addItem.js
@@ -0,0 +1,17 @@
+import ListItem from './ListItem';
+
+export default function addItem() {
+  const form = document.forms[1];
+  const list = document.querySelector('.list');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const item = form.item.value.trim();
+    if (item !== '') {
+      list.innerHTML += new ListItem(item).render();
+      form.reset();
+    }
+  })
+}
+
